6551d7da822eaa61ec30e7267e56503084f9f028
komalbodhankar <komal.bodhankar16@gmail.com>
2022-11-12 01:55:58 -0600

    adding updated node modules for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
+import { BrowserRouter, Route, Routes } from '../node_modules/react-router-dom/dist/index';
 import './App.css';
 import LeftNav from './components/LeftNav';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<LeftNav />} /><Route/>
+        <Route path="/" element={<LeftNav />} /><Route/>
         <Route path="/buildingPermit" element={<LeftNav />}></Route>
       </Routes>
     </BrowserRouter>
