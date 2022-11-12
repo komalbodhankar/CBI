7ec60d9812e8e83263824040951d4023eeebcf99
Rahul Sharma <rsbhatra98@gmail.com>
2022-11-12 07:40:39 -0600

    added es linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,31 @@
-import { Box, Container, Grid, Toolbar } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import BuildingPermit from './components/BuildingPermit';
-import LeftNav from './components/LeftNav';
-import * as React from 'react';
+import { Box, Container, Grid, Toolbar } from '@mui/material'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import BuildingPermit from './components/BuildingPermit'
+import LeftNav from './components/LeftNav'
+import * as React from 'react'
 
-
-function App() {
-  const [open, setOpen] = React.useState(false);
+function App () {
+  const [open, setOpen] = React.useState(false)
   const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   return (
-    
+
     <BrowserRouter>
       <Box sx={{ display: 'flex' }}>
         <LeftNav open={open} toggleDrawer={toggleDrawer} />
         <Box
           component="main"
           sx={{
-          backgroundColor: (theme) =>
+            backgroundColor: (theme) =>
               theme.palette.mode === 'light'
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
-          flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto',
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto'
           }}
       >
           <Toolbar />
@@ -35,13 +34,13 @@ function App() {
             <Routes>
               <Route path="/buildingPermit" element={<BuildingPermit />}></Route>
               <Route path="/unEmployment" element={<unEmployment />}></Route>
-            </Routes>      
+            </Routes>
           </Grid>
           </Container>
       </Box>
       </Box>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
