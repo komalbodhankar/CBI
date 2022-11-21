c4e3d852a771899f5f6bdb7420fab7c395f1ebd6
komalbodhankar <komal.bodhankar16@gmail.com>
2022-11-21 17:18:55 -0600

    updated ccvi code and removed lakshma.go file from git

--- go/src/github.com/cbi/api/main.go
+++ go/src/github.com/cbi/api/main.go
@@ -72,6 +72,31 @@ type healthHuman []struct {
 	HospitalizationsTotal string `json:"hospitalizations_total"`
 }
 
+type EthnicityCovid19 []struct {
+	LabReportDate                        string `json:"lab_report_date"`
+	CasesTotal                           string `json:"cases_total"`
+	DeathsTotal                          string `json:"deaths_total"`
+	HospitalizationsTotal                string `json:"hospitalizations_total"`
+	CasesLatinx                          string `json:"cases_latinx"`
+	CasesAsianNonLatinx                  string `json:"cases_asian_non_latinx"`
+	CasesBlackNonLatinx                  string `json:"cases_black_non_latinx"`
+	CasesWhiteNonLatinx                  string `json:"cases_white_non_latinx"`
+	CasesOtherNonLatinx                  string `json:"cases_other_non_latinx"`
+	CasesUnknownRaceEth                  string `json:"cases_unknown_race_eth"`
+	DeathsLatinx                         string `json:"deaths_latinx"`
+	DeathsAsianNonLatinx                 string `json:"deaths_asian_non_latinx"`
+	DeathsBlackNonLatinx                 string `json:"deaths_black_non_latinx"`
+	DeathsWhiteNonLatinx                 string `json:"deaths_white_non_latinx"`
+	DeathsOtherNonLatinx                 string `json:"deaths_other_non_latinx"`
+	DeathsUnknownRaceEth                 string `json:"deaths_unknown_race_eth"`
+	HospitalizationsLatinx               string `json:"hospitalizations_latinx"`
+	HospitalizationsAsianNonLatinx       string `json:"hospitalizations_asian_non_latinx"`
+	HospitalizationsBlackNonLatinx       string `json:"hospitalizations_black_non_latinx"`
+	HospitalizationsWhiteNonLatinx       string `json:"hospitalizations_white_non_latinx"`
+	HospitalizationsOtherRaceNonLatinx   string `json:"hospitalizations_other_race_non_latinx"`
+	HospitalizationsUnknownRaceEthnicity string `json:"hospitalizations_unknown_race_ethnicity"`
+}
+
 func main() {
 
 	connStr := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable"
@@ -493,4 +518,96 @@ func healthHumandB(db *sql.DB) {
 		}
 	}
 
+}
+
+func covidCCVI(db *sql.DB) {
+	response, err := http.Get("https://data.cityofchicago.org/resource/naz8-j4nc.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var CovidCCVIResponse EthnicityCovid19
+	json.Unmarshal(body, &CovidCCVIResponse)
+	fmt.Println(CovidCCVIResponse[0])
+
+	dropTable := `drop table if exists EthnicityCovid19`
+
+	_, dropErr := db.Exec(dropTable)
+
+	if dropErr != nil {
+		panic(err)
+	}
+
+	res, er := db.Exec("CREATE TABLE EthnicityCovid19 (LabReportDate VARCHAR ( 50 ),CasesTotal VARCHAR ( 50 )," +
+		"DeathsTotal VARCHAR ( 50 ),HospitalizationsTotal VARCHAR ( 50 )," +
+		"CasesLatinx VARCHAR ( 50 ),CasesAsianNonLatinx VARCHAR ( 50 )," +
+		"CasesBlackNonLatinx VARCHAR ( 50 ),CasesWhiteNonLatinx VARCHAR ( 50 )," +
+		"CasesOtherNonLatinx VARCHAR ( 50 ),CasesUnknownRaceEth VARCHAR ( 50 )," +
+		"DeathsLatinx VARCHAR ( 50 ),DeathsAsianNonLatinx VARCHAR ( 50 )," +
+		"DeathsBlackNonLatinx VARCHAR ( 50 ),DeathsWhiteNonLatinx VARCHAR ( 50 )," +
+		"DeathsOtherNonLatinx VARCHAR ( 50 ),DeathsUnknownRaceEth VARCHAR ( 50 )," +
+		"HospitalizationsLatinx VARCHAR ( 50 ),HospitalizationsAsianNonLatinx VARCHAR ( 50 )," +
+		"HospitalizationsBlackNonLatinx VARCHAR ( 50 ),HospitalizationsWhiteNonLatinx VARCHAR ( 50 )," +
+		"HospitalizationsOtherNonLatinx VARCHAR ( 50 ),HospitalizationsUnknownRaceEth VARCHAR ( 50 )," +
+		"createdAt TIMESTAMP WITH TIME ZONE NOT NULL," +
+		"updatedAt TIMESTAMP WITH TIME ZONE NOT NULL);")
+	if er != nil {
+		fmt.Println(er)
+		fmt.Println(res)
+	}
+
+	query := `INSERT INTO EthnicityCovid19(LabReportDate,
+		CasesTotal,
+		DeathsTotal,
+		HospitalizationsTotal,
+		CasesLatinx,
+		CasesAsianNonLatinx,
+		CasesBlackNonLatinx,
+		CasesWhiteNonLatinx,
+		CasesOtherNonLatinx,
+		CasesUnknownRaceEth,
+		DeathsLatinx,
+		DeathsAsianNonLatinx,
+		DeathsBlackNonLatinx,
+		DeathsWhiteNonLatinx,
+		DeathsOtherNonLatinx,
+		DeathsUnknownRaceEth,
+		HospitalizationsLatinx,
+		HospitalizationsAsianNonLatinx,
+		HospitalizationsBlackNonLatinx,
+		HospitalizationsWhiteNonLatinx,
+		createdAt,
+		updatedAt) 
+		VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22);`
+	for i := 0; i < len(CovidCCVIResponse); i++ {
+		_, err = db.Exec(query, CovidCCVIResponse[i].LabReportDate,
+			CovidCCVIResponse[i].CasesTotal,
+			CovidCCVIResponse[i].DeathsTotal,
+			CovidCCVIResponse[i].HospitalizationsTotal,
+			CovidCCVIResponse[i].CasesLatinx,
+			CovidCCVIResponse[i].CasesAsianNonLatinx,
+			CovidCCVIResponse[i].CasesBlackNonLatinx,
+			CovidCCVIResponse[i].CasesWhiteNonLatinx,
+			CovidCCVIResponse[i].CasesOtherNonLatinx,
+			CovidCCVIResponse[i].CasesUnknownRaceEth,
+			CovidCCVIResponse[i].DeathsLatinx,
+			CovidCCVIResponse[i].DeathsAsianNonLatinx,
+			CovidCCVIResponse[i].DeathsBlackNonLatinx,
+			CovidCCVIResponse[i].DeathsWhiteNonLatinx,
+			CovidCCVIResponse[i].DeathsOtherNonLatinx,
+			CovidCCVIResponse[i].DeathsUnknownRaceEth,
+			CovidCCVIResponse[i].HospitalizationsLatinx,
+			CovidCCVIResponse[i].HospitalizationsAsianNonLatinx,
+			CovidCCVIResponse[i].HospitalizationsBlackNonLatinx,
+			CovidCCVIResponse[i].HospitalizationsWhiteNonLatinx)
+		fmt.Println(i)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
