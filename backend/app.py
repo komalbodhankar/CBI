0644b1f3b0dfdf788f15ad1d2cb65a80743097da
komalbodhankar <komal.bodhankar16@gmail.com>
2022-11-30 01:24:54 -0600

    updated backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,6 @@
 from flask import Flask, jsonify
 import psycopg2
-from psycopg2 import Error
-from datetime import date
 from flask_cors import CORS
-import matplotlib
-import geopy
 import googlemaps
 import pandas as pd
 
@@ -13,35 +9,45 @@ import pandas as pd
 def get_zipcode(df, geolocator, lat_field, lon_field):
     location = geolocator.reverse((df[lat_field], df[lon_field]))
     return location.raw['address']['postcode']
+
+
 conn = None
 try:
     print('Connecting to PostgreSQL database...')
     conn = psycopg2.connect(
-    host="localhost",
-    database="chicago_business_intelligence",
-    user="postgres",
-    password="root")
+        host="localhost",
+        database="chicago_business_intelligence",
+        user="postgres",
+        password="root")
     cursor = conn.cursor()
     print('Cursor set...')
 except:
     print('Unable to connect to PostgreSQL connection URL...')
 
 app = Flask(__name__)
-CORS(app, resources = {r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 # logging.getLogger('flask_cors').level = logging.DEBUG
-CORS_ALLOW_ORIGINS = ["http://127.0.0.1:5000"] 
-
+CORS_ALLOW_ORIGINS = ["http://127.0.0.1:5000"]
 
 
 @app.route('/buildingPermit', methods=['GET'])
 def get_permits_data():
-    cursor.execute("select \"buildPermitId\", \"permitId\", \"permitType\", \"address\", \"zipcode\", \"areaName\", \"perCapita\", \"createdAt\", \"updatedAt\" from (select * from buildingpermits where \"permitType\" like 'PERMIT - NEW CONSTRUCTION%') permit left join (select \"areaCode\", \"areaName\", \"perCapita\" from unemployment_data) unemployment on (permit.\"communityarea\" = unemployment.\"areaCode\");")
+    cursor.execute(
+        "select \"buildPermitId\", \"permitId\", \"permitType\", \"address\", \"zipcode\", \"areaName\", "
+        "\"perCapita\", \"createdAt\", \"updatedAt\" from (select * from buildingpermits where \"permitType\" like "
+        "'PERMIT - NEW CONSTRUCTION%') permit left join (select \"areaCode\", \"areaName\", \"perCapita\" from "
+        "unemployment_data) unemployment on (permit.\"communityarea\" = unemployment.\"areaCode\");")
     data = cursor.fetchall()
     return jsonify(data)
 
+
 @app.route('/permitCountChart', methods=['GET'])
 def get_permit_charts_data():
-    cursor.execute("select \"permitType\", \"zipcode\", \"latitude\", \"longitude\",\"perCapita\" from (select * from buildingpermits where \"permitType\" like 'PERMIT - NEW CONSTRUCTION%') permit left join (select \"areaCode\", \"areaName\", \"perCapita\" from unemployment_data) unemployment on (permit.\"communityarea\" = unemployment.\"areaCode\");")
+    cursor.execute(
+        "select \"permitType\", \"zipcode\", \"latitude\", \"longitude\",\"perCapita\" from (select * from "
+        "buildingpermits where \"permitType\" like 'PERMIT - NEW CONSTRUCTION%') permit left join (select "
+        "\"areaCode\", \"areaName\", \"perCapita\" from unemployment_data) unemployment on (permit.\"communityarea\" "
+        "= unemployment.\"areaCode\");")
     data = cursor.fetchall()
     df = pd.DataFrame(data)
     df.columns = ['PermitType', 'ZipCode', 'Latitude', 'Longitude', 'PerCapita']
@@ -49,11 +55,11 @@ def get_permit_charts_data():
     result = []
     i = 0
     j = 0
-    j = i+1
+    j = i + 1
     for i in df.index:
         for j in df.index:
             if df['ZipCode'][i] == df['ZipCode'][j]:
-                count+=1
+                count += 1
         result.append(count)
         count = 0
     df['Count'] = result
@@ -62,37 +68,61 @@ def get_permit_charts_data():
     final_final = final.to_dict('records')
     return jsonify(final_final)
 
+
 @app.route('/emergencyLoan', methods=['GET'])
 def get_emergency_loan_latlng():
-    cursor.execute("select \"permitType\", \"zipcode\", \"latitude\", \"longitude\", \"perCapita\" from (select * from buildingpermits where \"permitType\" like 'PERMIT - NEW CONSTRUCTION%') permit left join (select \"areaCode\", \"areaName\", \"perCapita\" from unemployment_data) unemployment on (permit.\"communityarea\" = unemployment.\"areaCode\");")
+    cursor.execute(
+        "select \"permitType\", \"zipcode\", \"latitude\", \"longitude\", \"perCapita\" from (select * from "
+        "buildingpermits where \"permitType\" like 'PERMIT - NEW CONSTRUCTION%') permit left join (select "
+        "\"areaCode\", \"areaName\", \"perCapita\" from unemployment_data) unemployment on (permit.\"communityarea\" "
+        "= unemployment.\"areaCode\");")
     data = cursor.fetchall()
+
 
 @app.route('/unEmployment', methods=['GET'])
 def get_unemp_data():
-    cursor.execute('select "areaCode", "areaName", "belowPoverty" from unemployment_data order by "belowPoverty" desc limit 5;')
+    cursor.execute('select "areaCode", "areaName", "belowPoverty" from unemployment_data order by "belowPoverty" desc '
+                   'limit 5;')
     top5_poverty = cursor.fetchall()
-    cursor.execute('select "areaCode", "areaName", "unempRate" from unemployment_data order by "unempRate" desc limit 5;')
+    cursor.execute('select "areaCode", "areaName", "unempRate" from unemployment_data order by "unempRate" desc limit '
+                   '5;')
     top5_unemp = cursor.fetchall()
-    return jsonify(top5_poverty,top5_unemp)
+    return jsonify(top5_poverty, top5_unemp)
 
-@app.route('/getPoverty', methods = ['GET'])
+
+@app.route('/getPoverty', methods=['GET'])
 def get_address_Poverty():
-    cursor.execute('select * from (select "areaCode", "areaName", "belowPoverty" from unemployment_data order by "belowPoverty" desc limit 5) poverty left join (select "communityAreaNumber", "communityAreaZipCode" from community_area_zipcode) zipcode on (poverty."areaCode" = zipcode."communityAreaNumber");')
+    cursor.execute('select poverty."areaCode", poverty."areaName", poverty."belowPoverty", '
+                   'zipcode."communityAreaZipCode", sum(bp."totalwaived") as totalWaived from (select "areaCode", '
+                   '"areaName", '
+                   '"belowPoverty" from unemployment_data order by "belowPoverty" desc limit 5) poverty  left join '
+                   'community_area_zipcode zipcode on poverty."areaCode" = zipcode."communityAreaNumber" inner join '
+                   'buildingpermits bp on zipcode."communityAreaZipCode" = bp."zipcode" group by '
+                   'poverty."areaCode", poverty."areaName", poverty."belowPoverty", zipcode."communityAreaZipCode";')
     poverty = cursor.fetchall()
-    poverty_address = ""
-    latlng_poverty = {}
     poverty_array = []
     gmaps_key = googlemaps.Client(key='AIzaSyDr2sLloniItSejbFLVMShC9Kw0euajErY')
     for i in range(len(poverty)):
-        poverty_address = poverty[i][1] + ", " + "Chicago, " + "Illinois, " + poverty[i][4]
+        poverty_address = poverty[i][1] + ", " + "Chicago, " + "Illinois, " + poverty[i][3]
         geodecode_poverty = gmaps_key.geocode(poverty_address)
         latlng_poverty = geodecode_poverty[0]["geometry"]["location"]
+        latlng_poverty['areaCode'] = poverty[i][0]
+        latlng_poverty['areaName'] = poverty[i][1]
+        latlng_poverty['belowPoverty'] = poverty[i][2]
+        latlng_poverty['zipcode'] = poverty[i][3]
+        latlng_poverty['totalWaived'] = poverty[i][4]
         poverty_array.append(latlng_poverty)
     return jsonify(poverty_array)
 
-@app.route('/getUnemp', methods = ['GET'])
+
+@app.route('/getUnemp', methods=['GET'])
 def get_address_Unemp():
-    cursor.execute('select * from (select "areaCode", "areaName", "unempRate" from unemployment_data order by "unempRate" desc limit 5) poverty left join (select "communityAreaNumber", "communityAreaZipCode" from community_area_zipcode) zipcode on (poverty."areaCode" = zipcode."communityAreaNumber");')
+    cursor.execute('select poverty."areaCode", poverty."areaName", poverty."unempRate", '
+                   'zipcode."communityAreaZipCode", sum(bp."totalwaived") as totalWaived from  (select "areaCode", '
+                   '"areaName", "unempRate" from unemployment_data order by "unempRate" desc limit 5) poverty  left '
+                   'join community_area_zipcode zipcode on (poverty."areaCode" = zipcode."communityAreaNumber") inner '
+                   'join buildingpermits bp on (zipcode."communityAreaZipCode" = bp."zipcode") group by '
+                   'poverty."areaCode", poverty."areaName", poverty."unempRate", zipcode."communityAreaZipCode";')
     unemp = cursor.fetchall()
     unemp_address = ""
     latlng_unemp = {}
@@ -100,17 +130,24 @@ def get_address_Unemp():
     gmaps_key = googlemaps.Client(key='AIzaSyDr2sLloniItSejbFLVMShC9Kw0euajErY')
     for i in range(len(unemp)):
         print(unemp)
-        unemp_address = unemp[i][1] + ", " + "Chicago, " + "Illinois, " + unemp[i][4]
+        unemp_address = unemp[i][1] + ", " + "Chicago, " + "Illinois, " + unemp[i][3]
         geodecode_unemp = gmaps_key.geocode(unemp_address)
         latlng_unemp = geodecode_unemp[0]["geometry"]["location"]
+        latlng_unemp['areaCode'] = unemp[i][0]
+        latlng_unemp['areaName'] = unemp[i][1]
+        latlng_unemp['unempRate'] = unemp[i][2]
+        latlng_unemp['zipcode'] = unemp[i][3]
+        latlng_unemp['totalWaived'] = unemp[i][4]
         unemp_array.append(latlng_unemp)
     return jsonify(unemp_array)
-    
+
+
 @app.route('/ccvi', methods=['GET'])
 def get_ccvi_data():
     cursor.execute("SELECT * from ethnicitycovid19")
     data = cursor.fetchall()
     return jsonify(data)
+
 
 @app.route('/taxiTrips', methods=['GET'])
 def get_trips_data():
@@ -118,11 +155,13 @@ def get_trips_data():
     data = cursor.fetchall()
     return jsonify(data)
 
+
 @app.route('/covid19', methods=['GET'])
 def get_covid19_daily_data():
     cursor.execute("SELECT * from healthhumanservices")
     data = cursor.fetchall()
     return jsonify(data)
+
 
 @app.route('/covid19Zip', methods=['GET'])
 def get_covid19_weekly_data():
